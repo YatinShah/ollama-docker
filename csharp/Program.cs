4c5ce43a6c98a6a001a857182d0c156901b40202
Yatin Shah <yatin72@hotmail.com>
2024-12-19 23:35:37 -0600

    updt

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -12,10 +12,12 @@ Console.WriteLine("Hello, OpenAI-Ollama!");
 
 // api-key is not required for local server
 // so you can use any string here
-var openAIClient = new OpenAIClient(new ApiKeyCredential("api-key"), new OpenAIClientOptions
+var openAIClient = new OpenAIClient(new ApiKeyCredential("AnyText-NoRealKey"), new OpenAIClientOptions
 {
     Endpoint = new Uri("http://localhost:7869/"), // remember to add /v1/ at the end to connect to Ollama openai server
 });
+
+//make sure to install llama3.2:1b model through ollama container!!
 var model = "llama3.2:1b";
 
 var agent = new OpenAIChatAgent(
@@ -24,7 +26,8 @@ var agent = new OpenAIChatAgent(
     systemMessage: "You are a helpful assistant designed to output JSON.",
     seed: 0)
     .RegisterMessageConnector()
-    .RegisterPrintMessage();
+    .RegisterPrintMessage()
+    ;
 
 await agent.SendAsync("Can you write a piece of C# code to calculate 100th of fibonacci?");
 
